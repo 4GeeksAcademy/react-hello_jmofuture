c958776512b9ab33fc54aaee274ff3bec9a92aa2
Jean Olmedillo <78714438+Jmofuture@users.noreply.github.com>
2024-10-09 01:56:48 +0000

    descripciones ajustadas

--- src/js/component/cardsData.js
+++ src/js/component/cardsData.js
@@ -1,28 +1,29 @@
 const cardsData = [
     {
-        title: "Card 1",
-        description: "This is the description for card 1.",
+        title: "El Guerrero Solitario",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel sollicitudin libero, non tincidunt magna.",
         imageUrl: "https://th.bing.com/th/id/OIP.7noWuHYpKn8S5wZWp-W-OQHaEK?w=329&h=185&c=7&r=0&o=5&pid=1.7",
         url: "#"
     },
     {
-        title: "Card 2",
-        description: "This is the description for card 2.",
+        title: "El Anillo del Destino",
+        description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
         imageUrl: "https://th.bing.com/th/id/OIP.hhRa26p-EItkc6ycOT6AawHaEK?w=329&h=185&c=7&r=0&o=5&pid=1.7",
         url: "#"
     },
     {
-        title: "Card 3",
-        description: "This is the description for card 3.",
+        title: "El Vigilante del Castillo",
+        description: "Ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.",
         imageUrl: "https://th.bing.com/th/id/OIP.cnXE2ihw8h-0Y3CT8WZFsAHaEK?w=330&h=185&c=7&r=0&o=5&pid=1.7",
         url: "#"
     },
     {
-        title: "Card 4",
-        description: "This is the description for card 4.",
+        title: "El Árbol Sagrado",
+        description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         imageUrl: "https://th.bing.com/th/id/OIP.DGuQKxKzNcaIpi_DjKWKogHaEK?w=329&h=185&c=7&r=0&o=5&pid=1.7",
         url: "#"
     }
 ];
+
 
 export default cardsData;
